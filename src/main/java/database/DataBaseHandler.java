152b9adf68bbbb7d843f8bcb84af780e4c01dca7
Maureus <62718184+Maureus@users.noreply.github.com>
2020-08-23 15:48:33 +0200

    Update DataBaseHandler.java

--- src/main/java/database/DataBaseHandler.java
+++ src/main/java/database/DataBaseHandler.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DataBaseHandler {
-    private static final String USERNAME = "ST58211";
-    private static final String URL = "jdbc:oracle:thin:@fei-sql1.upceucebny.cz:1521:IDAS";
-    private static final String PASSWORD = "andr7265357";
+    private static final String USERNAME = "";
+    private static final String URL = "";
+    private static final String PASSWORD = "";
     private Connection dbConnection;
 
     public DataBaseHandler() {
